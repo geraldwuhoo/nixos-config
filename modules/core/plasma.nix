9cb7afee241551d2e2c7c511ac5e25c7fa90d8f0
geraldwuhoo <gerald@geraldwu.com>
2024-05-20 23:21:59 -0700

    remove konsole

--- modules/core/plasma.nix
+++ modules/core/plasma.nix
@@ -1,4 +1,4 @@
-{ ... }: {
+{ pkgs, ... }: {
   imports = [ ./x11.nix ];
 
   # display-manager
@@ -10,6 +10,9 @@
 
   # Enable Plasma 5 because Plasma 6 extension compatibility
   services.xserver.desktopManager.plasma5.enable = true;
+  environment.plasma5.excludePackages = with pkgs.libsForQt5; [
+    konsole # I don't need it since I have other terminal emulators
+  ];
 
   # Enable kwallet related things
   security.pam.services.jerry.enableKwallet = true;
