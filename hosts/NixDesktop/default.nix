88681114a1915cd3d67d65b329dbb8acb83b4385
geraldwuhoo <gerald@geraldwu.com>
2024-08-18 18:34:55 -0700

    disable profile-sync-daemon

--- hosts/NixDesktop/default.nix
+++ hosts/NixDesktop/default.nix
@@ -20,7 +20,7 @@
   nvidia.enable = true;
   openssh.enable = true;
   piper.enable = true;
-  psd.enable = true;
+  psd.enable = false;
   sunshine.enable = true;
   tor.enable = true;
   virtual.enable = true;
