c0c1b71a1b6aeb050b8c670bb6fdb49768daf352
geraldwuhoo <gerald@geraldwu.com>
2024-08-26 09:43:10 -0700

    add netavark systemd units to podman

--- modules/core/oci.nix
+++ modules/core/oci.nix
@@ -1,4 +1,9 @@
-{ lib, config, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
 {
   options = {
     oci.enable = lib.mkEnableOption "enables OCI containers";
@@ -14,5 +19,32 @@
     };
 
     hardware.nvidia-container-toolkit.enable = config.nvidia.enable;
+
+    systemd.sockets.netavark-dhcp-proxy = {
+      description = "Netavark DHCP proxy socket";
+      socketConfig = {
+
+        SocketGroup = "podman";
+        ListenStream = "%t/podman/nv-proxy.sock";
+        SocketMode = "0660";
+      };
+
+      wantedBy = [ "sockets.target" ];
+    };
+
+    systemd.services.netavark-dhcp-proxy = {
+      description = "Netavark DHCP proxy service";
+
+      serviceConfig = {
+        Type = "exec";
+        ExecStart = "${pkgs.netavark}/bin/netavark dhcp-proxy -a 30";
+      };
+
+      after = [ "netavark-dhcp-proxy.socket" ];
+      requires = [ "netavark-dhcp-proxy.socket" ];
+      startLimitIntervalSec = 0;
+
+      wantedBy = [ "default.target" ];
+    };
   };
 }
