3b29a91735407f8c637ae058eb9ab2de3ec518ee
geraldwuhoo <gerald@geraldwu.com>
2025-07-20 13:21:04 -0700

    update vscode deprecated options

--- home/vscode.nix
+++ home/vscode.nix
@@ -7,83 +7,85 @@
     enable = true;
     # Use unstable Codium package + extensions for latest versions
     package = pkgs.unstable.vscodium;
-    extensions = with pkgs.unstable.vscode-extensions; [
-      # Vim keybindings
-      vscodevim.vim
+    profiles.default = {
+      extensions = with pkgs.unstable.vscode-extensions; [
+        # Vim keybindings
+        vscodevim.vim
 
-      # Nix
-      jnoortheen.nix-ide
-      arrterian.nix-env-selector
-      mkhl.direnv
+        # Nix
+        jnoortheen.nix-ide
+        arrterian.nix-env-selector
+        mkhl.direnv
 
-      # Rust
-      rust-lang.rust-analyzer
-      tamasfe.even-better-toml
+        # Rust
+        rust-lang.rust-analyzer
+        tamasfe.even-better-toml
 
-      # GoLang
-      golang.go
+        # GoLang
+        golang.go
 
-      # Python
-      ms-python.python
-      ms-python.isort
-      ms-python.vscode-pylance # unfree
-      ms-python.black-formatter
+        # Python
+        ms-python.python
+        ms-python.isort
+        ms-python.vscode-pylance # unfree
+        ms-python.black-formatter
 
-      # Containers/Kubernetes
-      ms-kubernetes-tools.vscode-kubernetes-tools
-      ms-azuretools.vscode-docker
+        # Containers/Kubernetes
+        ms-kubernetes-tools.vscode-kubernetes-tools
+        ms-azuretools.vscode-docker
 
-      # IaC
-      hashicorp.terraform
-      redhat.vscode-yaml
-      redhat.ansible
-      signageos.signageos-vscode-sops # unfree (no LICENSE in upstream)
+        # IaC
+        hashicorp.terraform
+        redhat.vscode-yaml
+        redhat.ansible
+        signageos.signageos-vscode-sops # unfree (no LICENSE in upstream)
 
-      # Enhanced text
-      mechatroner.rainbow-csv
-      yzhang.markdown-all-in-one
+        # Enhanced text
+        mechatroner.rainbow-csv
+        yzhang.markdown-all-in-one
 
-      # Theming
-      arcticicestudio.nord-visual-studio-code
-      pkief.material-icon-theme
-    ];
+        # Theming
+        arcticicestudio.nord-visual-studio-code
+        pkief.material-icon-theme
+      ];
 
-    userSettings = {
-      # Theming
-      "workbench.colorTheme" = "Nord";
-      "workbench.iconTheme" = "material-icon-theme";
+      userSettings = {
+        # Theming
+        "workbench.colorTheme" = "Nord";
+        "workbench.iconTheme" = "material-icon-theme";
 
-      # Mandatory for high refresh screens
-      "editor.cursorBlinking" = "smooth";
-      "editor.cursorSmoothCaretAnimation" = "on";
-      "editor.smoothScrolling" = true;
-      "workbench.list.smoothScrolling" = true;
-      "terminal.integrated.smoothScrolling" = true;
+        # Mandatory for high refresh screens
+        "editor.cursorBlinking" = "smooth";
+        "editor.cursorSmoothCaretAnimation" = "on";
+        "editor.smoothScrolling" = true;
+        "workbench.list.smoothScrolling" = true;
+        "terminal.integrated.smoothScrolling" = true;
 
-      # D E N S I T Y
-      "editor.fontSize" = 14;
-      "editor.inlayHints.fontSize" = 10;
-      "editor.minimap.enabled" = false;
-      "editor.wordWrap" = "on";
-      "window.menuBarVisibility" = "toggle";
-      "window.zoomLevel" = -1.5;
+        # D E N S I T Y
+        "editor.fontSize" = 14;
+        "editor.inlayHints.fontSize" = 10;
+        "editor.minimap.enabled" = false;
+        "editor.wordWrap" = "on";
+        "window.menuBarVisibility" = "toggle";
+        "window.zoomLevel" = -1.5;
 
-      # vim niceties
-      "editor.lineNumbers" = "relative";
-      "vim.smartRelativeLine" = true;
-      "vim.easymotion" = true;
-      "vim.leader" = "<space>";
+        # vim niceties
+        "editor.lineNumbers" = "relative";
+        "vim.smartRelativeLine" = true;
+        "vim.easymotion" = true;
+        "vim.leader" = "<space>";
 
-      # nix
-      "nix.enableLanguageServer" = true;
-      "nix.serverPath" = "nil";
-      "nix.formatterPath" = "/run/current-system/sw/bin/nixfmt";
+        # nix
+        "nix.enableLanguageServer" = true;
+        "nix.serverPath" = "nil";
+        "nix.formatterPath" = "/run/current-system/sw/bin/nixfmt";
 
-      # disable trash
-      "files.enableTrash" = false;
+        # disable trash
+        "files.enableTrash" = false;
 
-      # Begone telemetry
-      "redhat.telemetry.enabled" = false;
+        # Begone telemetry
+        "redhat.telemetry.enabled" = false;
+      };
     };
   };
 }
