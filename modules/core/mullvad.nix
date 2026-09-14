fa35799c7548ee99a61e84ef12ad440c566775c5
geraldwuhoo <gerald@geraldwu.com>
2026-09-13 23:37:31 -0700

    route lan subnets via main table so they don't depend on mullvad

--- modules/core/mullvad.nix
+++ modules/core/mullvad.nix
@@ -1,4 +1,16 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
+let
+  lanSubnets = [
+    "10.10.0.0/24"
+    "10.20.0.0/24"
+    "10.30.0.0/24"
+    "10.40.0.0/24"
+    "10.50.0.0/24"
+    "10.60.0.0/24"
+    "10.70.0.0/24"
+    "172.16.16.0/24"
+  ];
+in
 {
   # Get Mullvad browser as well
   environment.systemPackages = with pkgs; [ mullvad-browser ];
@@ -19,17 +31,26 @@
         content = ''
           chain excludeOutgoing {
             type route hook output priority 0; policy accept;
-            ip daddr 10.10.0.0/24 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
-            ip daddr 10.20.0.0/24 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
-            ip daddr 10.30.0.0/24 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
-            ip daddr 10.40.0.0/24 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
-            ip daddr 10.50.0.0/24 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
-            ip daddr 10.60.0.0/24 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
-            ip daddr 10.70.0.0/24 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
-            ip daddr 172.16.16.0/24 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
+            ${lib.concatMapStringsSep "\n  " (
+              subnet: "ip daddr ${subnet} ct mark set 0x00000f41 meta mark set 0x6d6f6c65;"
+            ) lanSubnets}
           }
         '';
       };
+    };
+  };
+
+  # The nft reroute happens after the source address is picked from mullvad's table,
+  # leaving LAN connections bound to the tunnel IP; route them via main up front instead
+  systemd.services.lan-bypass-mullvad = {
+    description = "Route LAN subnets outside Mullvad";
+    wantedBy = [ "multi-user.target" ];
+    before = [ "network.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = map (subnet: "${pkgs.iproute2}/bin/ip rule add to ${subnet} lookup main priority 32000") lanSubnets;
+      ExecStop = map (subnet: "-${pkgs.iproute2}/bin/ip rule del to ${subnet} lookup main priority 32000") lanSubnets;
     };
   };
 }
